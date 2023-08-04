71c443d060e1c8f9fd8f6db829c611e0fbb5b2db
vedprakash <gsumit765@gmail.com>
2023-08-05 01:02:50 +0530

    adding entity class

--- src/main/java/com/git_example/SpringBootApp1Application.java
+++ src/main/java/com/git_example/SpringBootApp1Application.java
@@ -1,15 +1,15 @@
 package com.git_example;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootApp1Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext run = SpringApplication.run(SpringBootApp1Application.class, args);
-		run.close();
+		SpringApplication.run(SpringBootApp1Application.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/git_example/entity/Student.java
@@ -0,0 +1,7 @@
+package com.git_example.entity;
+
+public class Student {
+	public static void main(String[] args) {
+
+	}
+}
